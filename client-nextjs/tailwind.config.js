4a56a8b9749977ce8ce724113588731b95d39c6d
Peter Mutwiri <84850942+PETERMUTWIRI@users.noreply.github.com>
2025-10-16 16:21:39 +0000

    added postconfig  debug plugin

--- client-nextjs/tailwind.config.js
+++ client-nextjs/tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
     },
   },
   plugins: [
+    require('./postcss-debug-length.js'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('tailwindcss-animate'),
