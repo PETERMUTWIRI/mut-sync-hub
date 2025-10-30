b43ec46ba2928b8f70837778cbe98a2f30730fd4
Peter Mutwiri <84850942+PETERMUTWIRI@users.noreply.github.com>
2025-10-30 11:35:53 +0000

    revert to standard Next build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import { StackProvider, StackTheme } from '@stackframe/stack';
 import { stackClientApp } from '@/lib/stack.client';
 
 import { Toaster } from 'react-hot-toast';
-import '@/app/global.css'
+import './global.css';
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
