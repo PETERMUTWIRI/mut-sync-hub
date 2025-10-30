4f1569034a87e2450d5b2815f6edbf946d71bef6
Peter Mutwiri <84850942+PETERMUTWIRI@users.noreply.github.com>
2025-10-30 12:02:52 +0000

    added tailwind css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { StackProvider, StackTheme } from '@stackframe/stack';
 import { stackClientApp } from '@/lib/stack.client';
 
 import { Toaster } from 'react-hot-toast';
-import './global.css';
+import '@/app/tailwind.css';
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
